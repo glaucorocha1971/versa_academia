74f43b9a0cf45a25ab62979adefbbc60b82695ce
fabriciosantosm <fabriciosantos30@hotmail.com>
2022-12-28 17:55:52 -0300

    update

--- gc/resources/views/empresas.blade.php
+++ gc/resources/views/empresas.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Versa Empresas</title>
+    <title> Versa Empresas </title>
 </head>
 <body>
     <!doctype html>
@@ -15,121 +15,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <style>
-        #geral{
-            margin: 0;
-            padding: 0;
-            display: flex;
-        }
-        #naveUm{
-            width: 50%;
-            height: 800px;
-            display: flex;
-            align-items: center;
-            flex-direction: column;
-        }
-        #logo{
-            height: 100px;
-            width: 90%;
-            background-color: red;)
-        }
-
-        #perfil{
-            display: flex;
-        }
-        #fotoPerfil{
-            width: 100px;
-            height: 100px;
-            border-radius: 100px;
-            background-color: white;
-            margin-right: 10px;
-        }
-        .form-label{
-            color: #fdb735;
-            font-weight: bold;
-        }
-        .form-control{
-            background-color: #66c2ff;
-            width: 600px;
-            height: 40px;
-        }
-        #imagemlogo{
-            width: 100%;
-            height: 100px;
-        }
-        #faixada{
-            width: 100%;
-            height: 50px;
-            background-color: black;
-            color: white;
-            font-size: 30px;
-            text-align: center;
-        }
-        #okbutton{
-            width: 100%;
-            height: 50px;
-            background-color: black;
-            border-radius: 50px;
-            margin-top: 15px;
-            font-weight: bold;
-        }
-        #empresas{
-            background-color: #000000;
-            width: 50%;
-            height: 800px;
-        }
-        #empresaicon{
-            width: 200px;
-            height: 50px;
-            float: left;
-            margin: 30px;
-            color: #ffffff;
-            font-size: 30px
-
-        }
-        #plusbutton{
-            width: 40px;
-            height: 40px;
-            float: right;
-            border-radius: 100px;
-            background-color: #ffffff;
-            color: #000000;
-            font-weight: bold;
-            margin: 5px;
-            padding: 0px;
-            font-size: 20px;
-        }
-        #pesquisa{
-            width: 480px;
-            height: 30px;
-            float: left;
-            margin: 5px;
-            margin-left: 30px;
-            background-color: #ffffff;
-            border-radius: 0;
-            margin-top: 15px;
-            margin-bottom: 10px;
-        }
-        #contversatec{
-            width: 480px;
-            height: 200px;
-            float: left;
-            background-color: #ffffff;
-            margin: 30px    
-        }
-        .empresascaixa{
-            width: 60%;
-            height: 1px;
-            margin: 20px;
-            float: left;
-        }
-        #editarempresas{
-            width: 10px;
-            height: 10px;
-            float: left;
-        }
-    </style>
-    <title>Home</title>
+    <link rel="stylesheet" href="styles/layout.css" type="text/css">
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js"> </script>
+    <title> Versa Empresas </title>
 </head>
 <body id="geral">
     <div id="naveUm">
@@ -148,19 +36,19 @@
             </div>
             <div class="mb-3">
                 <label for="CNPJ" class="form-label">< CNPJ ></label>
-                <input type="number" class="form-control" id="CNPJ">
+                <input type="text" class="form-control" id="CNPJ">
             </div>
             <div class="mb-3">
-                <label for="E-mail" class="form-label">< E-mail ></label>
-                <input type="email" class="form-control" id="E-mail">
+                <label for="email" class="form-label">< E-mail ></label>
+                <input type="email" class="form-control" id="email">
             </div>
             <div class="mb-3">
-                <label for="Whatsapp" class="form-label">< Whatsapp ></label>
-                <input type="tel" class="form-control" id="Whatsapp">
+                <label for="whatsapp" class="form-label">< Whatsapp ></label>
+                <input type="tel" class="form-control" id="whatsapp">
             </div>
             <div class="mb-3">
                 <label for="Telefone" class="form-label">< Telefone ></label>
-                <input type="tel" class="form-control" id="Telefone">
+                <input type="tel" class="form-control" id="telefone">
             </div>
             <button type="submit" class="btn btn-primary" id="okbutton">OK</button>
         </nav>
@@ -182,7 +70,16 @@
         </div>
 
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>   
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script src="scripts/jquery.mask.min.js"></script>
+    <script>
+        $(document).ready(function(){
+            $('#cep').mask('00.000-000');
+            $('#telefone').mask('+00 (00) 0 0000-0000');
+            $('#whatsapp').mask('+00 (00) 0 0000-0000');
+            $('#CNPJ').mask('00.000.000/0000-00');
+        });
+    </script>
+    </body>
 </html>
 
--- gc/resources/views/home.blade.php
+++ gc/resources/views/home.blade.php
@@ -5,53 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <style>
-        #geral{
-            margin: 0;
-            padding: 0;
-            display: flex;
-        }
-        #naveUm{
-            width: 50%;
-            height: 1000px;
-            display: flex;
-            align-items: center;
-            flex-direction: column;
-        }
-        #logo{
-            height: 100px;
-            width: 90%;
-            background-color: red;
-        }
-        ul{
-            list-style-type: none;
-        }
-        li{
-            font-size: 20pt;
-            font-family: sans-serif;
-        }
-        li a{
-            text-decoration: none;
-            color: black;
-        }
-        li a:hover{
-            opacity: 0.3;
-        }
-        .linkSecundario{
-            font-size: 17pt;
-        }
-        #perfil{
-            display: flex;
-        }
-        #fotoPerfil{
-            width: 100px;
-            height: 100px;
-            border-radius: 100px;
-            background-color: white;
-            margin-right: 10px;
-        }
-    </style>
-    <title>Home</title>
+    <title> Home </title>
+    <link rel="stylesheet" href="styles/layout.css" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <body id="geral">
     <div id="naveUm">
@@ -61,7 +17,7 @@
         <nav>
             <ul>
                 <li><a href="#">< > Versa Empresas</a></li>
-                <li>< > Versa Ocupações</li>
+                <li>< > Versa Ocupações </li>
                 <li>
                     <ul>
                         <li><a class="linkSecundario" href="#">< > Versa Formação</a></li>
@@ -91,4 +47,5 @@
         </div>
     </div>
 </body>
+<script src="scripts/jquery.mask.min.js"> </script>
 </html>
